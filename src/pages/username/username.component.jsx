f83df0fd3e5a0775bc5e84d8f3a640cf6db38244
Krishna <krishnacaresu@gmail.com>
2020-06-26 22:30:50 +0530

    modified username page and added modal to it

--- src/pages/username/username.component.jsx
+++ src/pages/username/username.component.jsx
@@ -1,38 +1,46 @@
 import React from 'react'
-import { Form, Input } from 'semantic-ui-react'
+import { Form, Input, Modal } from 'semantic-ui-react'
 import { setLoggedUser } from '../../redux/details/details.actions'
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class Username extends React.Component {
 
-    state = { username: '' }
+    state = { username: '', open: true }
 
     handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
     handleSubmit = () => {
         const { history } = this.props
         const { username } = this.state
-        this.setState({ username }, () => {
+        this.setState({ username, open: false }, () => {
             this.props.setLoggedUser(this.state.username)
             history.push('/home')
         })
     }
 
     render() {
-        const { username } = this.state
+        const { username, open } = this.state
         return (
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group inline>
-                    <Form.Field required width={12}>
-                        <Input name='username'
-                            value={username}
-                            onChange={this.handleChange}
-                            placeholder='Enter username' />
-                    </Form.Field>
-                    <Form.Button floated='right' width={4}>Save</Form.Button>
-                </Form.Group>
-            </Form>
+
+            <Modal open={open} closeOnEscape='false'
+                closeOnDimmerClick='false'>
+                <Modal.Header>Enter username</Modal.Header>
+                <Modal.Content>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group inline>
+                            <Form.Field required width={12}>
+                                <Input name='username'
+                                    value={username}
+                                    onChange={this.handleChange}
+                                    placeholder='Enter username' />
+                            </Form.Field>
+                            <Form.Button floated='right' width={4}>Save</Form.Button>
+                        </Form.Group>
+                    </Form>
+                </Modal.Content>
+            </Modal>
+
         )
     }
 }
